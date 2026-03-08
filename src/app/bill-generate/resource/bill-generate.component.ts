5d5cbf9b2de6f2ad0ce5134c0265a246d3665098
palanichamy <palanichamytextiles@gmail.com>
2026-03-08 22:58:51 +0530

    angular 18 -revert

--- src/app/bill-generate/resource/bill-generate.component.ts
+++ src/app/bill-generate/resource/bill-generate.component.ts
@@ -28,8 +28,8 @@ import { AlertService } from '../../alert-service/service/alert.service';
   hxncode = '55095300'
   taxpercenatage = 5;
   sgtcgt = this.taxpercenatage/2;
-  vehicleno = '';
-  // vehicleno = 'TN 49 L 2160'
+  // vehicleno = '';
+  vehicleno = 'TN 49 L 2160'
   // vehicleno = 'TN 76 AF 4359'
   gstvaluefind: any;
   getBillData:any = [];
@@ -215,8 +215,7 @@ import { AlertService } from '../../alert-service/service/alert.service';
   }
   exportAsPDF(pdfOption: any, pdfData: any) {
     const serialno = pdfData.customer_bill_no;
-    const vanTransport = 'VAN';
-    const modetransport = (pdfData.customer_mode_transport == 1) ? vanTransport : (pdfData.customer_mode_transport == 2) ? 'Jeyavilas' : (pdfData.customer_mode_transport == 3) ? 'TRMS LORRY' : 'VAN';
+    const modetransport = (pdfData.customer_mode_transport == 1) ? 'VAN' : (pdfData.customer_mode_transport == 2) ? 'Jeyavilas' : (pdfData.customer_mode_transport == 3) ? 'TRMS LORRY' : 'VAN';
     const vehicleNo = (pdfData.customer_mode_transport == 1 && this.vehicleno) ? this.vehicleno : '..............................';
     const billdate = pdfData.customer_bill_date;
     const noofbag = pdfData.customer_no_of_bag;
